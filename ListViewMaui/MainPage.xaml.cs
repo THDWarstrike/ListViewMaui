197395cfb8b58d2bd68fbaf86f43da848d694e1a
Keith McHugh <mchughkeith3@gmail.com>
2022-11-30 08:53:17 -0500

    Add project files.

---
+++ ListViewMaui/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace ListViewMaui;
+
+public partial class MainPage : ContentPage
+{
+	List<string> animallist = new List<string>()
+	{
+		"Dog", "Rat", "Walrus","Llama","Hippopotamus", "Bear", "Tiger", "Lemur","Lion", "Megaladon"
+	};
+
+	public MainPage()
+	{
+		InitializeComponent();
+		LvofAnimals.ItemsSource = animallist;
+	}
+
+
+}
+
